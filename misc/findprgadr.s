7c4a1e48279e066ea456eb6ce2e2e1a99f8ac5e6
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2020-05-02 20:04:27 +0200

    * Extend SEI/CLI guard & fix for page boundaries.

--- misc/findprgadr.s
+++ misc/findprgadr.s
@@ -3,18 +3,21 @@
 ; find and print program address
 ; works by calling into a rom routine, causing the sp to be saved.
 
-*= $6000
+*= $c0f7
 
 linprt = $bdcd
 
-findadr         jsr $f5ae ; rts
+findadr         sei                     ; stop irqs from touching the stack
+                jsr $f5ae               ; rts
                 tsx
-                .byte $bd,$ff,$00 ; lda $00ff,x (force abs. x addr. mode)
-                ldy $0100,x ; high byte
-printadr        tax ; store low-byte in x
-                dex ; address returned is 2+, so
-                dex ; decrement twice
-                tya ; store high-byte in a
-                jsr linprt
+                .byte $bd,$ff,$00       ; lda $00ff,x (force abs. x addr.)
+                ldy $0100,x             ; high-byte
+                cli
+fixadr          sec                     ; subtract 3 to fix return address
+                sbc #3
+                tax                     ; put low-byte in x
+                tya                     ; put high-byte in acc.
+                sbc #0
+printadr        jsr linprt
 quit            rts
 
