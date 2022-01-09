480dad411514a8a91ca4c831b4e38d496d842338
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2022-01-09 18:33:51 +0100

    Draw borders

--- neuralnet/brainsim.s
+++ neuralnet/brainsim.s
@@ -25,19 +25,49 @@ printPrompt:    lda prompt
                 cmp #<promptEnd
                 bne printPrompt
 
+                drawBorder(4, 5)
+                drawBorder(4, 25)
+
                 rts
 
+            .macro drawBorder(r1,c1) {
+                .var v
+                .var vv
+                .for (var i=0; i<2; i++) {
+                    .eval v = $0400+$28*(r1+i*8)+c1
+                    .eval vv = $70-3*i
+                    lda #vv
+                    sta v
+
+                    .for (var j=1; j<9; j++) {
+                        lda #$43
+                        sta v+j
+                    }
+
+                    .eval vv = $6e+$f*i
+                    lda #vv
+                    sta v+9
+                }
+
+                .for (var i=1; i<8; i++) {
+                    .eval v = $0400+$28*(r1+i)+c1
+                    lda #$5d
+                    sta v
+                    sta v+9
+                }
+            }
+                
                 // Variable declarations
 f1:             .fill 42, 0
 f2:             .fill 42, 0
-m:              .fill 42*42,0
+m:              .fill 42*42, 0
 v:              .byte 0
 j:              .word 0
 i:              .word 0
 prompt:         .text @"    neuron network associative memory\n\n"
-                .fill 12,$11            // cursor down
+                .fill 12, $11            // cursor down 12 times
                 .text "f1 - teach pattern     "
-                .text @"f2 - dump matrixq\n"
+                .text @"f2 - dump matrix\n"
                 .text "f3 - randomize pattern "
                 .text @"f4 - forget all\n"
                 .text "f5 - recall pattern    "
