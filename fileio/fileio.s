1f09529361de223572201ad68482cbb606220890
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2023-12-26 19:29:27 +1030

    Add file output capability

--- fileio/fileio.s
+++ fileio/fileio.s
@@ -2,65 +2,95 @@
 	.const setnam = $ffbd
 	.const open = $ffc0
 	.const chkin = $ffc6
+	.const chkout = $ffc9
 	.const chrin = $ffcf
+	.const chrout = $ffd2
 	.const close = $ffc3
 	.const clrchn = $ffcc
-	.const status = $90
-	.const zp = $fb
-	.const buffer = $0801
-	.const fnam = "0:sequentials,s,r"
+	.const status = $90	
+
+	.const bufsz = $4000	// size of text buffer: 16K
+	.const bufptr = $fb	// pointer to text buffer
+	.const fnmptr = $fd	// pointer to zero-terminated filename
 
 	*= $c000
 
-main:
-	ldx #<buffer
-	ldy #>buffer
-	jsr readbf
-	rts
-
 	// open a seq or prg file and read all data into a buffer.
-	// enter with address of storage buffer in x (lo) and y (hi).
-	// upon return x and y will hold end-of-buffer address.
+	// enter with fnmptr pointing at zero-terminated filename and
+	// bufptr pointing at text buffer. upon return x and y will
+	// hold end-of-buffer address.
 readbf:
-	stx zp
-	sty zp+1
 	jsr openfl
 	jsr readfl
 	lda #1
 	jsr closfl
-	ldx zp
-	ldy zp+1
+	ldx bufptr
+	ldy bufptr+1
 	rts
 
-	// open a seq or prg file for reading/writing.
+	// open a seq or prg file and write all data from
+	// zero-terminated buffer to the file. enter with fnmptr
+	// pointing at zero-terminated filename and bufptr pointing at
+	// text buffer.
+writbf:
+	jsr openfl
+	jsr writfl
+	lda #1
+	jsr closfl
+	rts
+
+	// open a seq or prg file for reading/writing. enter with
+	// fnmptr pointing at zero-terminated filename.
 openfl:
 	lda #1
 	ldx #8
 	ldy #2
 	jsr setlfs
-	lda #fnleng
-	ldx #<filenm
-	ldy #>filenm
+	jsr fnamsz
+	ldx fnmptr
+	ldy fnmptr+1
 	jsr setnam
 	jsr open
 	rts
 
 	// read a character from a seq or prg file and store in buffer
-	// whose address is in zp.
-readfl:
+	// whose address is in bufptr. enter with bufptr pointing at
+	// text buffer.
+readfl: {
 	ldx #1
 	jsr chkin
 	ldy #0
-rdloop:
+loop:
 	jsr chrin
-	sta (zp),y
-	inc zp
+	sta (bufptr),y
+	inc bufptr
 	bne statck
-	inc zp+1
+	inc bufptr+1
 statck:
 	lda status
-	beq rdloop
+	beq loop
 	rts
+}
+	
+	// write a character to a seq or prg file from zero-terminated
+	// buffer whose address is in bufptr. enter with bufptr
+	// pointing at text buffer.
+writfl: {
+	ldx #1
+	jsr chkout
+	ldy #0
+loop:
+	lda (bufptr),y
+	beq done
+	jsr chrout
+	inc bufptr
+	bne more
+	inc bufptr+1
+more:	
+	jmp loop
+done:
+	rts
+}	
 
 	// close the logical file specified in acc and restore device
 	// values.
@@ -68,5 +98,21 @@ closfl:
 	jsr close
 	jmp clrchn
 
-filenm:	.text fnam
-fnleng:	.byte fnam.size()
+	// get size of filename. call with fnmptr set to point to
+	// zero-terminated filename. upon return acc contains size of
+	// filename.
+fnamsz: {
+	ldx #0
+loop:	
+	lda (fnmptr),y
+	beq done
+	inx
+	inc fnmptr
+	bne more
+	inc fnmptr+1
+more:
+	jmp loop
+done:
+	txa
+	rts
+}
